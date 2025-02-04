7fe1097f085e7dcb2a755cac353525dcdc89c8ec
dashakots <dkatsuba.work@gmail.com>
2025-02-04 11:18:17 +0300

    comment 3

--- lesson17/index17.js
+++ lesson17/index17.js
@@ -11,26 +11,33 @@ btnDown.onclick = async () => {
 products.forEach(product => { 
     const divCard = document.createElement('div');
     divCard.classList.add('card');
-    const img = document.createElement('image');
+    const img = document.createElement('img');
     img.classList.add('image');
+    img.src = product.image;
     const h5 = document.createElement('h5');
     h5.classList.add('title');
-    h5.innerText = product.title;
+    h5.innerText = product.title.slice(0, 30) + '...';
     const p1 = document.createElement('p'); 
     p1.classList.add('category');
-    p1.innerText = product.category;  
+    p1.innerText = product.category;
+    p1.style.color = '#4b4453';
     const p2 = document.createElement('p');
     p2.classList.add('description');
-    p2.innerText = product.description;
+    p2.innerText = product.description.slice(0, 50) + '...';
+    p2.style.color = '#845ec2';
     const p3 = document.createElement('p');
     p3.classList.add('price');
     p3.innerText = `$${product.rating.count}`;
+    p3.style.color = '#b0a8b9';
     const a = document.createElement('a');
     a.classList.add('toSee');
     a.innerText = 'See More';
+    a.style.color = "#c34a36";
+
+    
 
     a.onclick = () => {
-        console.log(console.log(product.id, product.title));
+        console.log(product.id, product.title);
         };
     
         divCard.append(img, h5, p1, p2, p3, a);
@@ -38,4 +45,6 @@ products.forEach(product => {
 
 });
 };
+
+
 
